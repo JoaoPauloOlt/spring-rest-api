3b6a4c2fc63603a2ae079ba1e59936e5f9cb7756
João Paulo <jpoltramari@gmail.com>
2026-04-16 13:01:36 -0300

    test: configura Maven Failsafe plugin no projeto

--- src/test/java/com/algaworks/algafood/RegisterKitchenIT.java
+++ src/test/java/com/algaworks/algafood/RegisterKitchenIT.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RegisterKitchenIntegrationTests {
+public class RegisterKitchenIT {
 
     @Autowired
     private RegisterKitchenService registerKitchen;
