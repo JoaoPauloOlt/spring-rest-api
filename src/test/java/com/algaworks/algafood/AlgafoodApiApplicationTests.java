df692faf99f91f9916fc9c4db1671b6ede1ccf89
João Paulo <jpoltramari@gmail.com>
2026-04-16 12:10:12 -0300

    test: configura ambiente para testes de integração

--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,9 +1,18 @@
 package com.algaworks.algafood;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.fail;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+public class AlgafoodApiApplicationTests {
 
-
+    @Test
+    public void contextLoads(){
+        fail();
+    }
 }
