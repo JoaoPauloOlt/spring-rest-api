7dc0dee7e5fe922574916424eff5dad68c08a21d
João Paulo <jpoltramari@gmail.com>
2026-04-26 11:01:47 -0300

    refactor: refatora os controllers para que usem os Inputs e Models criados

--- src/main/java/com/algaworks/algafood/api/controller/StateController.java
+++ src/main/java/com/algaworks/algafood/api/controller/StateController.java
@@ -1,9 +1,12 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.api.assembler.StateInputDisassembler;
+import com.algaworks.algafood.api.assembler.StateModelAssembler;
+import com.algaworks.algafood.api.model.StateModel;
+import com.algaworks.algafood.api.model.input.StateInput;
 import com.algaworks.algafood.domain.model.State;
 import com.algaworks.algafood.domain.repository.StateRepository;
 import com.algaworks.algafood.domain.service.RegisterStateService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,29 +24,44 @@ public class StateController {
     @Autowired
     private RegisterStateService registerState;
 
+    @Autowired
+    private StateModelAssembler stateModelAssembler;
+
+    @Autowired
+    private StateInputDisassembler stateInputDisassembler;
+
     @GetMapping
-    public List<State> list(){
-        return stateRepository.findAll();
+    public List<StateModel> list(){
+        List<State> allStates = stateRepository.findAll();
+
+        return stateModelAssembler.toCollectionModel(allStates);
     }
 
     @GetMapping("/{stateId}")
-    public State search(@PathVariable Long stateId){
-        return registerState.searchOrError(stateId);
+    public StateModel search(@PathVariable Long stateId){
+        State state = registerState.searchOrError(stateId);
+
+        return stateModelAssembler.toModel(state);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public State add(@RequestBody @Valid State state){
-        return registerState.save(state);
+    public StateModel add(@RequestBody @Valid StateInput stateInput){
+        State state = stateInputDisassembler.toDomainObject(stateInput);
+
+        state = registerState.save(state);
+
+        return stateModelAssembler.toModel(state);
     }
 
     @PutMapping("/{stateId}")
-    public State update(@PathVariable Long stateId, @RequestBody @Valid State state){
+    public StateModel update(@PathVariable Long stateId, @RequestBody @Valid StateInput stateInput){
         State stateActual = registerState.searchOrError(stateId);
 
-        BeanUtils.copyProperties(state, stateActual, "id");
+        stateInputDisassembler.copyToDomainObject(stateInput, stateActual);
+        stateActual = registerState.save(stateActual);
 
-        return registerState.save(stateActual);
+        return stateModelAssembler.toModel(stateActual);
     }
 
     @DeleteMapping("/{stateId}")
