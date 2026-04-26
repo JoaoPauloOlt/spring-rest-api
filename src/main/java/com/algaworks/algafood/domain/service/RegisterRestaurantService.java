1280e993cc186173a606a86f469587b3039feba9
João Paulo <jpoltramari@gmail.com>
2026-04-26 11:29:02 -0300

    fix: Corrigindo a importação das transações

--- src/main/java/com/algaworks/algafood/domain/service/RegisterRestaurantService.java
+++ src/main/java/com/algaworks/algafood/domain/service/RegisterRestaurantService.java
@@ -7,8 +7,7 @@ import com.algaworks.algafood.domain.repository.KitchenRepository;
 import com.algaworks.algafood.domain.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RegisterRestaurantService {
