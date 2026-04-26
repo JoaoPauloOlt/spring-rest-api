5ac7bbb19828e4566b4c8d1ce8ca9fb6d16ae38a
João Paulo <jpoltramari@gmail.com>
2026-04-26 11:00:00 -0300

    fix: remove a anotação @NotBlank da entidade Cozinha

--- src/main/java/com/algaworks/algafood/domain/model/Kitchen.java
+++ src/main/java/com/algaworks/algafood/domain/model/Kitchen.java
@@ -1,14 +1,11 @@
 package com.algaworks.algafood.domain.model;
 
-import com.algaworks.algafood.core.validation.Groups;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +20,6 @@ public class Kitchen {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
     @Column(nullable = false)
     private String name;
 
