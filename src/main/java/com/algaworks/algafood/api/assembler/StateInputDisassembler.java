b2108ee14879105f678c7b8d83fcf810a2abefc8
João Paulo <jpoltramari@gmail.com>
2026-04-26 10:58:01 -0300

    feature: cria os disassemblers

---
+++ src/main/java/com/algaworks/algafood/api/assembler/StateInputDisassembler.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.api.assembler;
+
+import com.algaworks.algafood.api.model.input.StateInput;
+import com.algaworks.algafood.domain.model.State;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StateInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public State toDomainObject(StateInput stateInput){
+        return modelMapper.map(stateInput, State.class);
+    }
+
+    public void copyToDomainObject(StateInput stateInput, State state){
+        modelMapper.map(stateInput, state);
+    }
+}
