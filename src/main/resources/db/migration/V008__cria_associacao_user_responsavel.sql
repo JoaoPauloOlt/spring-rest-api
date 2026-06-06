01b87dc18ef29b4f0a585f0cce021c3fa9c9111f
João Paulo <jpoltramari@gmail.com>
2026-06-06 11:26:43 -0300

    fix: Arrumando erro de inicialização do flyway

--- src/main/resources/db/migration/V008__cria_associacao_user_responsavel.sql
+++ src/main/resources/db/migration/V008__cria_associacao_user_responsavel.sql
@@ -1,8 +1,9 @@
 create table restaurant_user_responsible (
     restaurant_id bigint not null,
     user_id bigint not null,
+
     primary key (restaurant_id, user_id)
-) engine=InnoDB default charset=utf8;
+);
 
 alter table restaurant_user_responsible add constraint fk_restaurant_user_restaurant
 foreign key (restaurant_id) references restaurant (id);
